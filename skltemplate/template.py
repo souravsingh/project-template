20a2f9b565e6d9a8998c474f62c6b25b637f1173
Sourav Singh <4314261+souravsingh@users.noreply.github.com>
2018-09-10 19:22:24 +0530

    Update template.py

--- skltemplate/template.py
+++ skltemplate/template.py
@@ -2,22 +2,52 @@
 This is a module to be used as a reference for building other modules
 """
 import numpy as np
+import scipy.sparse as sp
+
 from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.extmath import row_norms, squared_norm, stable_cumsum
 from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics.pairwise import pairwise_distances_argmin_min
+from sklearn.utils.sparsefuncs_fast import assign_rows_csr
+from sklearn.utils.sparsefuncs import mean_variance_axis
+from sklearn.utils.validation import _num_samples
+from sklearn.utils import check_array
+from sklearn.utils import check_random_state
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import FLOAT_DTYPES
+from sklearn.utils import Parallel
+from sklearn.utils import delayed
+from sklearn.utils import effective_n_jobs
+from sklearn.externals.six import string_types
+from sklearn.exceptions import ConvergenceWarning
+from sklearn import _k_means
+from sklearn_k_means_elkan import k_means_elkan
 from sklearn.metrics import euclidean_distances
+from __future__ import division
+import warnings
+
 
 
 class TemplateEstimator(BaseEstimator):
-    """ A template estimator to be used as a reference implementation .
-
-    Parameters
-    ----------
-    demo_param : str, optional
-        A parameter used for demonstation of how to pass and store paramters.
-    """
-    def __init__(self, demo_param='demo_param'):
-        self.demo_param = demo_param
+   
+    def __init__(self, X, kmax=20, n_jobs=None, verbose=0):
+        self.X = X
+        self.num = np.size(self.X, axis=0)
+        self.dim = np.size(X, axis=1)
+        self.n_jobs = n_jobs
+        self.verbose = verbose
+        self.KMax = kmax
+        
+    def _check_test_data(self, X):
+        X = check_array(X, accept_sparse='csr', dtype=FLOAT_DTYPES)
+        n_samples, n_features = X.shape
+        expected_n_features = self.cluster_centers_.shape[1]
+        if not n_features == expected_n_features:
+            raise ValueError("Incorrect number of features. "
+                             "Got %d features, expected %d" % (
+                                 n_features, expected_n_features))
 
     def fit(self, X, y):
         """A reference implementation of a fitting function
